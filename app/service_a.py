fd29b1918970e46c2e6dad0b2ba08f61c8f5ae40
ele-003 <dlwndud003@gmail.com>
2025-02-19 17:25:33 +0900

    Update service_a.py port number

--- app/service_a.py
+++ app/service_a.py
@@ -25,7 +25,7 @@ DB_NAME = "test"
 DB_USER = "postgres"
 DB_PASSWORD = "1234"
 DB_HOST = "localhost"
-DB_PORT = "5433"
+DB_PORT = "5432"
 
 # Clova Speech Long Sentence API 설정
 NAVER_CLOVA_SECRET_KEY = "clova-key"
